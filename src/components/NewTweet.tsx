e62bc72cb619961673bafa7fabc918c12e0e28a4
srijan <srijandubey3105@gmail.com>
2023-05-17 23:42:10 +0530

    new tweet section + variying height + added styles

--- src/components/NewTweet.tsx
+++ src/components/NewTweet.tsx
@@ -1,13 +1,39 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import Button from './Button'
+import ProfilePicture from './ProfilePicture'
+import { useSession } from 'next-auth/react'
+import { useState } from 'react'
+
+function updateTextAreaHeight(textarea: HTMLTextAreaElement) {
+    if (textarea == null) return
+    textarea.style.height = "0"
+    textarea.style.height = `${textarea.scrollHeight}px`
+}
 
 function NewTweet() {
+    const session = useSession()
+    const [text, setText] = useState('')
+    const textAreaRef = useRef<HTMLTextAreaElement>(null)
+
+
+
+    useEffect(() => {
+        updateTextAreaHeight
+    }, [text])
+
+    if (session.status !== 'authenticated' || session.data?.user == null) return null
+
     return (
         <form className='flex flex-col gap-2 border-b px-4 py-2'>
             <div className='flex gap-4 '>
-                {/* <ProfilePicture /> */}
+                <ProfilePicture src={session.data.user.image} />
                 {/* text area */}
-                <textarea  className='flex-grow resize-none overflow-hidden p-4 text-lg outline-none' placeholder='What is happening?!'/>
+                <textarea
+                    ref={textAreaRef}
+                    onChange={e => setText(e.target.value)}
+                    value={text}
+                    style={{ height: 'max-content' }}
+                    className='flex-grow resize-none  overflow-hidden p-4 text-lg outline-none' placeholder='What is happening?!' />
             </div>
             <Button className='self-end' >Tweet</Button>
         </form>
