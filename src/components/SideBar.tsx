d10e2e85991da72e8036cea243213e896e595b81
srijan <srijandubey3105@gmail.com>
2023-05-20 23:32:08 +0530

    customized side nav bar component

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,6 +1,8 @@
 import Link from 'next/link'
 import React, { use } from 'react'
 import { useSession, signOut, signIn } from 'next-auth/react'
+import { IconHoverEffect } from './IconHoverEffect'
+import { VscAccount, VscHome, VscSignIn, VscSignOut } from 'react-icons/vsc'
 
 
 function SideBar() {
@@ -8,24 +10,55 @@ function SideBar() {
     const user = session.data?.user
 
     return (
-        <nav className="sticky top-0 self-start px-2 py-4 ml-28">
-
-            <ul className='flex  flex-col items-start gap-2 whitespace-nowrap '>
+        <nav className="sticky top-0 px-2 py-4">
+            <ul className="flex flex-col items-start gap-2 whitespace-nowrap">
                 <li>
-                    <Link href="/" >Home</Link>
+                    <Link href="/">
+                        <IconHoverEffect>
+                            <span className="flex items-center gap-4">
+                                <VscHome className="h-8 w-8" />
+                                <span className="hidden text-lg md:inline">Home</span>
+                            </span>
+                        </IconHoverEffect>
+                    </Link>
                 </li>
-                {user != null && (<li>
-                    <Link href={`/profiles/${user.id}`} >Profile</Link>
-                </li>)}
-
-                {user == null ? (
-
+                {user != null && (
                     <li>
-                        <button onClick={() => void signIn()}> Log In</button>
+                        <Link href={`/profiles/${user.id}`}>
+                            <IconHoverEffect>
+                                <span className="flex items-center gap-4">
+                                    <VscAccount className="h-8 w-8" />
+                                    <span className="hidden text-lg md:inline">Profile</span>
+                                </span>
+                            </IconHoverEffect>
+                        </Link>
+                    </li>
+                )}
+                {user == null ? (
+                    <li>
+                        <button onClick={() => void signIn()}>
+                            <IconHoverEffect>
+                                <span className="flex items-center gap-4">
+                                    <VscSignIn className="h-8 w-8 fill-green-700" />
+                                    <span className="hidden text-lg text-green-700 md:inline">
+                                        Log In
+                                    </span>
+                                </span>
+                            </IconHoverEffect>
+                        </button>
                     </li>
                 ) : (
                     <li>
-                        <button onClick={() => void signOut()}> Log out</button>
+                        <button onClick={() => void signOut()}>
+                            <IconHoverEffect>
+                                <span className="flex items-center gap-4">
+                                    <VscSignOut className="h-8 w-8 fill-red-700" />
+                                    <span className="hidden text-lg text-red-700 md:inline">
+                                        Log Out
+                                    </span>
+                                </span>
+                            </IconHoverEffect>
+                        </button>
                     </li>
                 )}
             </ul>
