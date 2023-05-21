3c88aa235393e12e38de04c3e907a84ea192f977
srijan <srijandubey3105@gmail.com>
2023-05-21 14:03:05 +0530

    using trpc utils to add optimistic updates to the like counter

--- src/components/ListOFtweets.tsx
+++ src/components/ListOFtweets.tsx
@@ -4,6 +4,8 @@ import ProfilePicture from "./ProfilePicture"
 import { useSession } from 'next-auth/react'
 import { VscHeart, VscHeartFilled } from 'react-icons/vsc'
 import { IconHoverEffect } from "./IconHoverEffect"
+import { api } from "~/utils/api"
+
 
 type Tweet = {
     id: string
@@ -28,9 +30,12 @@ type InfiniteTweetListProps = {
 }
 
 type HeartButton = {
+    isLoading: boolean
+    onClick: () => void
     likedByMe: boolean
     likeCount: number
 }
+
 
 
 export default function ListOFtweets({ tweets, isError, isLoading, fetchNextPage, hasMore }: InfiniteTweetListProps) {
@@ -58,6 +63,43 @@ export default function ListOFtweets({ tweets, isError, isLoading, fetchNextPage
 
 
 function TweetCard({ id, user, content, createdAt, likeCount, liked }: Tweet) {
+
+    const trpcUtils = api.useContext()
+    const toggleLike = api.tweet.toggleLike.useMutation({
+        onSuccess: ({ addedLike }) => {
+            const updateData: Parameters<typeof trpcUtils.tweet.infiniteScroll.setInfiniteData>[1] = (oldData) => {
+                if (oldData == null) return
+
+                const count = addedLike ? 1 : -1
+                return {
+                    ...oldData,
+                    pages: oldData.pages.map((page) => {
+                        return {
+                            ...page,
+                            tweets: page.tweets.map((tweet) => {
+                                if (tweet.id === id) {
+                                    return {
+                                        ...tweet,
+                                        likeCount: tweet.likeCount + count,
+                                        liked: addedLike,
+                                    }
+                                }
+                                return tweet
+                            })
+                        }
+                    })
+                }
+
+            }
+
+            trpcUtils.tweet.infiniteScroll.setInfiniteData({}, updateData)
+        },
+    })
+    const handleLike = () => {
+        toggleLike.mutate({ id })
+    }
+
+
     return <li className="flex gap-4 border-b px-4 py-4 ">
         <Link href={`/profiles/${user.id}`}>
             <ProfilePicture src={user.image} />
@@ -71,31 +113,36 @@ function TweetCard({ id, user, content, createdAt, likeCount, liked }: Tweet) {
                 <span className="text-gray-500">{dateTimeFormatter.format(createdAt)}</span>
             </div>
             <p className="whitespace-pre-wrap">{content}</p>
-            <HeartButton likedByMe={liked} likeCount={likeCount} />
+            <HeartButton onClick={handleLike} isLoading={toggleLike.isLoading} likedByMe={liked} likeCount={likeCount} />
 
         </div>
     </li>
 }
 const dateTimeFormatter = new Intl.DateTimeFormat(undefined, { dateStyle: "short" })
 
-function HeartButton({ likedByMe, likeCount }: HeartButton) {
+function HeartButton({ isLoading, onClick, likedByMe, likeCount }: HeartButton) {
     const session = useSession()
     const HeartIcon = likedByMe ? VscHeartFilled : VscHeart
 
     if (session.status !== 'authenticated') {
-        return (<div className="m-1 flex items-center gap-3 self-start text-gray-500">
+        return (<div className="mb-1 mt-1 flex items-center gap-3 self-start text-gray-500">
             <HeartIcon />
             <span>{likeCount}</span>
         </div>)
     }
     return (
-        <button className={`group items-center gap-1 self-start flex transition-colors duration-200 
-        ${likedByMe ? "text-red-500" : "text-gray-500 hover:text-red-500 focus-visible:text-red-500"}`}>
+        <button
+            disabled={isLoading}
+            onClick={onClick}
+            className={`group -ml-2 flex items-center gap-1 self-start transition-colors duration-200 ${likedByMe
+                ? "text-red-500"
+                : "text-gray-500 hover:text-red-500 focus-visible:text-red-500"
+                }`}
+        >
             <IconHoverEffect red>
-
                 <HeartIcon
                     className={`transition-colors duration-200 ${likedByMe
-                        ? "fill-red-500"
+                        ? "fill-red-500 "
                         : "fill-gray-500 group-hover:fill-red-500 group-focus-visible:fill-red-500"
                         }`}
                 />
